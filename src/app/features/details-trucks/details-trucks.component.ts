b4ef93a83a2a4a98e7e82eab214181a65fec46a6
davsalgonz <david.slmnc@gmail.com>
2023-04-08 12:58:38 +0200

    fin susmuertos 4h

--- src/app/features/details-trucks/details-trucks.component.ts
+++ src/app/features/details-trucks/details-trucks.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Trucks } from '../models/trucks';
+import { ActivatedRoute } from '@angular/router';
+
+import { Services7trucksService } from '../services/services7trucks.service';
 
 @Component({
   selector: 'app-details-trucks',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./details-trucks.component.scss']
 })
 export class DetailsTrucksComponent {
+  productData! : Trucks;
 
+  constructor(private route : ActivatedRoute, private goods : Services7trucksService) {
+    this.route.params.subscribe(params => {
+      this.productData = this.goods.getProduct(params['id']);
+      console.log(this.productData);
+    })
+   }
 }
